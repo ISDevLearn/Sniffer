ebf5017c6786119b1d7842cc3f9ba09c7a3f3c5e
颜琪滨 <2535367985@qq.com>
2021-11-28 20:42:00 +0800

    basically complete sniffer main thread

--- source/sniffer.py
+++ source/sniffer.py
@@ -30,20 +30,83 @@ class Sniffer:
     def stop(self):
         self.sniffer.stop()
 
+    def get_protocol(self, p: Packet):
+        protocol_list = p.summary().split('/')
+        # protocol_list:  ['Ether ', ' IP ', ' TCP 192.168.31.253:8051 > 175.27.204.206:https FA']
+        print(protocol_list)
+        arp_protocol_list = ['ARP', 'RARP', 'DHCP']
+        # arp protocol_list:  ['Ether ', ' ARP who has 192.168.31.1 says 192.168.31.253']
+        for protocol in arp_protocol_list:
+            if protocol in protocol_list[1]:
+                return protocol
+        if 'IP' in protocol_list[1]:
+            if 'Raw' in protocol_list[-1] or 'Padding' in protocol_list[-1]:
+                upper_protocol = protocol_list[-2]
+            else:
+                upper_protocol = protocol_list[-1]
+            return upper_protocol.strip().split(' ')[0]
+        # ipv6:
+        # ['Ether ', ' IPv6 ', ' UDP ', ' DNS Ans "fe80::10e1:13bd:be7:8c38" ']
+        elif 'IPv6' in protocol_list[1]:
+            return 'IPv6/' + protocol_list[2].strip().split(' ')[0]
+        # DNS and others
+        else:
+            protocol = protocol_list[2].strip().split(' ')[0]
+            if protocol != '':
+                protocol += '/'
+            protocol += protocol_list[2].split(' ')[1]
+            return protocol
+
+    def get_info(self, p: Packet):
+        protocol_list = p.summary().split("/")
+        if "Ether" in protocol_list[0]:
+            protocol = self.get_protocol(p)
+            # arp protocol_list:  ['Ether ', ' ARP who has 192.168.31.1 says 192.168.31.253']
+            # ['Ether ', ' ARP is at 54:48:e6:99:c9:1c says 192.168.31.1']
+            if 'ARP' in protocol:
+                ARP_info = protocol_list[1].strip().split(' ')[1:]
+                if ARP_info[0] == 'who' and ARP_info[1] == 'has':
+                    info = "Who has " + ARP_info[2] + "? Tell " + ARP_info[4]
+                elif ARP_info[0] == 'is' and ARP_info[1] == 'at':
+                    info = ARP_info[4] + "is at " + ARP_info[2]
+                else:
+                    info = protocol_list[1].strip()
+            # DNS protocol_list:  ['Ether ', ' IP ', ' UDP ', ' DNS Qry "b\'lb._dns-sd._udp.local.\'" ']
+            elif 'DNS' in protocol:
+                info = protocol_list[-1]
+            # tcp/udp ['Ether ', ' IP ', ' UDP 192.168.31.253:54915 > 172.19.83.255:54915 ', ' Raw']
+            elif 'TCP' in protocol or 'UDP' in protocol:
+                info = protocol_list[2]
+            else:
+                ip_list = protocol_list[1:]
+                info = ""
+                for s in ip_list:
+                    info += s
+            return info
+        else:
+            return p.summary()
+
+    def get_src_and_dst(self, p: Packet):
+        if p.haslayer('IP'):
+            src = p['IP'].src
+            dst = p['IP'].dst
+        else:
+            src = p[0].src
+            dst = p[0].dst
+        return src, dst
+
     def handle(self, p: Packet):
         self.number += 1
         data = p.show(dump=True)
         # print(p['IP'].show(dump=True))
         # print(p.haslayer('IP'))
         # print(res)
-        packet_time = p.time-self.time
-        src = ''
-        dst = ''
-        if p.haslayer('IP'):
-            src = p['IP'].src
-            dst = p['IP'].dst
-            print(src)
-            print(dst)
-        signals.update_table.emit([self.number, packet_time, src, dst, 'tmp', 'tmp', 'tmp'])
+        packet_time = str(p.time-self.time)[0:9]
+        src, dst = self.get_src_and_dst(p)
+        protocol = self.get_protocol(p)
+        length = len(p)
+        info = self.get_info(p)
+
+        signals.update_table.emit([self.number, packet_time, src, dst, protocol, length, info])
 
 
--- source/utils.py
+++ source/utils.py
@@ -15,15 +15,17 @@ def modify(_ui: QWidget):
     ui = _ui
     set_table(ui.table)
     get_nif(ui.if_box)  # 获取网卡
-    set_toolbar()
-    set_signal()
+    set_toolbar()  # 设置工具栏操作
+    set_signal()  # 设置信号
 
 
+# 获取网卡
 def get_nif(if_box: QComboBox):
     if_list = [nif.name for nif in get_working_ifaces() if nif.mac]
     if_box.addItems(if_list)
 
 
+# 设置信息展示表格
 def set_table(table: QTableWidget):
     table.horizontalHeader().setSectionResizeMode(QHeaderView.Interactive)
     table.horizontalHeader().setStretchLastSection(True)
@@ -32,17 +34,22 @@ def set_table(table: QTableWidget):
     QTableWidget.resizeRowsToContents(table)
 
 
+# 设置工具栏操作
 def set_toolbar():
     ui.action_exit.triggered.connect(exit)
     ui.action_exit_2.triggered.connect(exit)
-    ui.action_start.triggered.connect(start)  # test
+    ui.action_start.triggered.connect(start)
+    ui.action_start_2.triggered.connect(start)
     ui.action_stop.triggered.connect(stop)
+    ui.action_stop_2.triggered.connect(stop)
 
 
+# 设置信号
 def set_signal():
     signals.update_table.connect(add_row)
 
 
+# 退出界面
 def exit():
     reply = QMessageBox.question(ui, '温馨提示',
                                  "确定退出吗?",
@@ -51,6 +58,7 @@ def exit():
         ui.close()
 
 
+# 添加行
 def add_row(info: list):
     table: QTableWidget = ui.table
     rows = table.rowCount()
@@ -61,6 +69,7 @@ def add_row(info: list):
     table.scrollToBottom()
 
 
+# 开始嗅探
 def start():
     global s
     # signals.update_table.emit([1, 2, 3, 4, 5, 6, 7])
@@ -68,5 +77,6 @@ def start():
     s.start()
 
 
+# 停止嗅探
 def stop():
     s.stop()
